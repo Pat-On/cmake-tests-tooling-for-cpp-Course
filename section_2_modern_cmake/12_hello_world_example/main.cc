2f37c141f40894dd746ef36bdb99c07bfa93e564
Patryk <patryk.r.nowak@gmail.com>
2023-05-01 17:52:02 +0100

    Hello world - cmake

---
+++ section_2_modern_cmake/12_hello_world_example/main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+// CMake - is the abstraction that is going to tell compile from where to take the libraries that we want to use
+int main () 
+{
+    std::cout << "Hello World \n";
+
+    return 0;
+
+}
