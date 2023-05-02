f0a713cdb6f048b3b667ae6939ccf6869c84cbbc
Patryk <patryk.r.nowak@gmail.com>
2023-05-02 08:14:47 +0100

    config.hpp.in

---
+++ section_2_modern_cmake/18_configure_file/app/main.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "my_lib.h" // compilation unit
+#include "config.hpp"
+
+
+int main () 
+{
+    std::cout << project_name << "\n";
+    std::cout << project_version << "\n";
+
+
+    print_hello_world();
+
+    return 0;
+
+}
