6a8db845b3b97e003e7c8cf268ce23943b1a2fd1
Patryk <patryk.r.nowak@gmail.com>
2023-05-06 22:04:25 +0100

    doxygen

---
+++ section_2_modern_cmake/24_doxygen_documentation/src/my_lib/my_lib.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "my_lib.h"
+
+#include <nlohmann/json.hpp>
+
+// compilation unit / library
+
+/**
+ * @brief Prints out hello world and tests the JSON
+*/
+void print_hello_world (){
+        std::cout << "Hello World \n";
+        std::cout << "JSON Lib Version:"
+                << NLOHMANN_JSON_VERSION_MAJOR << "."
+                << NLOHMANN_JSON_VERSION_MINOR << "."
+                << NLOHMANN_JSON_VERSION_PATCH << "\n";
+}
