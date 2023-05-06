cded11d6118a9934bf22596f555929acbd69c2d8
Patryk <patryk.r.nowak@gmail.com>
2023-05-06 08:47:32 +0100

    21

---
+++ section_2_modern_cmake/21_external_libraries_with_gitsubmodule/cmake/AddGitSubmodule.cmake
@@ -0,0 +1,17 @@
+# Function definition
+
+function(add_git_submodule dir)
+# is going to try find the git
+    find_package(Git REQUIRED)
+
+    if (NOT EXISTS ${dir}/CMakeLists.txt)
+        # set by previous line 5
+        execute_process(COMMAND ${GIT_EXECUTABLE}
+            submodule update --init --recursive -- ${dir}
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+    endif()
+
+    # is adding the library to target list and its cmake file
+    add_subdirectory(${dir})
+
+endfunction(add_git_submodule)
---
+++ section_2_modern_cmake/21_external_libraries_with_gitsubmodule/src/my_lib/my_lib.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "my_lib.h"
+
+
+// compilation unit / library
+
+void print_hello_world (){
+        std::cout << "Hello World \n";
+}
